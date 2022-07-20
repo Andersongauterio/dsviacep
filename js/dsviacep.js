cae80dc45a994c1e219803e62627b3346a628f1d
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-07-19 21:16:41 -0300

    open close modal contact

--- js/dsviacep.js
+++ js/dsviacep.js
@@ -1,5 +1,9 @@
 import * as formController from './controllers/form-controller.js';
 import * as listController from './controllers/list-controller.js';
+import * as moodalController from './controllers/modal-controller.js';
+import * as pageController from './controllers/page-controller.js';
 
 formController.init();
 listController.init();
+moodalController.init();
+pageController.init();
